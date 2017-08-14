41be0e9b176202a4d95b398c4bf35063a6fe95dc
Ananta11 <ananta.shahane@aol.com>
2017-08-14 13:46:47 +0530

    Initial Commit

---
+++ PeerReview11 v2/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  PeerReview11 v2
+//
+//  Created by Ananta Shahane on 14/08/17.
+//  Copyright © 2017 Ananta Shahane. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ PeerReview11 v2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PeerReview11 v2
+//
+//  Created by Ananta Shahane on 14/08/17.
+//  Copyright © 2017 Ananta Shahane. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
